f57b2c3d6e100d2f992aad1d9a51422ebc39653d
Aaron E <mettamara@gmail.com>
2013-09-27 18:38:28 -0700

    adding explicit hasher and plan example

--- scripts/reconfiguration/loadAndMig.sh
+++ scripts/reconfiguration/loadAndMig.sh
@@ -1,2 +1,2 @@
-ant hstore-benchmark -Dproject=tpcc -Dnosites=true -Dnoexecute=true
+ant hstore-benchmark -Dproject=tpcc -Dnosites=true -Dnoexecute=true -Dglobal.hasher_plan=test.json -Dglobal.hasherClass=edu.brown.hashing.PlannedHasher
 ant hstore-invoke -Dproject=tpcc -Dproc=@Reconfiguration -Dparam0=0 -Dparam1=2 -Dparam2=livepull
