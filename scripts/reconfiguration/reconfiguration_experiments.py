7ac2cf408fdca3df26c41decb2b686621d102f0d
Aaron J Elmore <aelmore@fiction.cs.ucsb.edu>
2013-10-28 18:16:53 -0700

    updated

--- scripts/reconfiguration/reconfiguration_experiments.py
+++ scripts/reconfiguration/reconfiguration_experiments.py
@@ -6,8 +6,8 @@ RECONFIG_EXPERIMENTS = [
 def updateReconfigurationExperimentEnv(fabric, args, benchmark, partitions ):
     partitions_per_site = fabric.env["hstore.partitions_per_site"]
     if args['exp_type'] == 'reconfig-baseline':
-        fabric.env["client.blocking_concurrent"] = 5 # * int(partitions/8)
+        fabric.env["client.blocking_concurrent"] = 4 # * int(partitions/8)
         fabric.env["client.count"] = 8
         fabric.env["client.blocking"] = True
         fabric.env["client.output_response_status"] = True
-        fabric.env["client.threads_per_host"] = max(50, int(partitions * 4))
+        fabric.env["client.threads_per_host"] = min(50, int(partitions * 4))
