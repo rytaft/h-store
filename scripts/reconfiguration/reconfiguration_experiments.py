7140a85872e3e28421f7fa8c91dd01dd2d4d63ea
Aaron Elmore <aelmore@cs.ucsb.edu>
2013-11-30 12:44:48 -0800

    fixes to tests

--- scripts/reconfiguration/reconfiguration_experiments.py
+++ scripts/reconfiguration/reconfiguration_experiments.py
@@ -14,18 +14,20 @@ RECONFIG_EXPERIMENTS = [
     "reconfig-localhost",
 ]
 
+RECONFIG_CLIENT_COUNT = 1
+
 def updateReconfigurationExperimentEnv(fabric, args, benchmark, partitions ):
     partitions_per_site = fabric.env["hstore.partitions_per_site"]
     if args['exp_type'] == 'reconfig-baseline':
         fabric.env["client.blocking_concurrent"] = 4 # * int(partitions/8)
-        fabric.env["client.count"] = 8
+        fabric.env["client.count"] = RECONFIG_CLIENT_COUNT
         fabric.env["client.blocking"] = True
         fabric.env["client.output_response_status"] = True
         fabric.env["client.threads_per_host"] = min(50, int(partitions * 4))
     
     if args['exp_type'] == 'reconfig-localhost':
         fabric.env["client.blocking_concurrent"] = 4 # * int(partitions/8)
-        fabric.env["client.count"] = 1
+        fabric.env["client.count"] = RECONFIG_CLIENT_COUNT
         fabric.env["client.blocking"] = True
         fabric.env["client.output_response_status"] = True
         fabric.env["client.threads_per_host"] = 2
@@ -35,7 +37,7 @@ def updateReconfigurationExperimentEnv(fabric, args, benchmark, partitions ):
     
     if 'reconfig-wh-baseline' in args['exp_type']:
         fabric.env["client.blocking_concurrent"] = 5 # * int(partitions/8)
-        fabric.env["client.count"] = 8
+        fabric.env["client.count"] = RECONFIG_CLIENT_COUNT
         fabric.env["client.blocking"] = True
         fabric.env["client.output_response_status"] = True
         fabric.env["client.threads_per_host"] = min(50, int(partitions * 4))
@@ -43,67 +45,78 @@ def updateReconfigurationExperimentEnv(fabric, args, benchmark, partitions ):
     
     if 'reconfig-.5' in args['exp_type']:
         fabric.env["client.blocking_concurrent"] = 5 # * int(partitions/8)
-        fabric.env["client.count"] = 1#4
+        fabric.env["client.count"] = RECONFIG_CLIENT_COUNT#4
         fabric.env["client.blocking"] = True
         fabric.env["client.output_response_status"] = True
         fabric.env["client.threads_per_host"] = min(50, int(partitions * 4))
         fabric.env["site.reconfig_chunk_size_kb"] = 512
+        fabric.env["site.reconfig_async_chunk_size_kb"] = 512
         fabric.env["site.commandlog_enable"] = False
 
     if 'reconfig-1' in args['exp_type']:
         fabric.env["client.blocking_concurrent"] = 5 # * int(partitions/8)
-        fabric.env["client.count"] = 1#41#
+        fabric.env["client.count"] = RECONFIG_CLIENT_COUNT
         fabric.env["client.blocking"] = True
         fabric.env["client.output_response_status"] = True
         fabric.env["client.threads_per_host"] = min(50, int(partitions * 4))
         fabric.env["site.reconfig_chunk_size_kb"] = 1024
+        fabric.env["site.reconfig_async_chunk_size_kb"] = 1024
         fabric.env["site.commandlog_enable"] = False
 
     if 'reconfig-2' in args['exp_type']:
         fabric.env["client.blocking_concurrent"] = 5 # * int(partitions/8)
-        fabric.env["client.count"] = 1#4
+        fabric.env["client.count"] = RECONFIG_CLIENT_COUNT
         fabric.env["client.blocking"] = True
         fabric.env["client.output_response_status"] = True
         fabric.env["client.threads_per_host"] = min(50, int(partitions * 4))
         fabric.env["site.reconfig_chunk_size_kb"] = 2048 
+        fabric.env["site.reconfig_async_chunk_size_kb"] = 2048
+        fabric.env["site.reconfig_async_chunk_size_kb"] = 2048
         fabric.env["site.commandlog_enable"] = False
     
     if 'reconfig-4' in args['exp_type']:
         fabric.env["client.blocking_concurrent"] = 5 # * int(partitions/8)
-        fabric.env["client.count"] = 1#4
+        fabric.env["client.count"] = RECONFIG_CLIENT_COUNT
         fabric.env["client.blocking"] = True
         fabric.env["client.output_response_status"] = True
         fabric.env["client.threads_per_host"] = min(50, int(partitions * 4))
         fabric.env["site.commandlog_enable"] = False
         fabric.env["site.reconfig_chunk_size_kb"] = 4096
+        fabric.env["site.reconfig_async_chunk_size_kb"] = 4096
 
     if 'reconfig-10' in args['exp_type']:
         fabric.env["client.blocking_concurrent"] = 5 # * int(partitions/8)
-        fabric.env["client.count"] = 1#4
+        fabric.env["client.count"] = RECONFIG_CLIENT_COUNT
         fabric.env["client.blocking"] = True
         fabric.env["client.output_response_status"] = True
         fabric.env["client.threads_per_host"] = min(50, int(partitions * 4))
         fabric.env["site.commandlog_enable"] = False
         fabric.env["site.reconfig_chunk_size_kb"] = 10000
+        fabric.env["site.reconfig_async_chunk_size_kb"] = 10000
+
 
     if 'stopcopy-1' in args['exp_type']:
         fabric.env["client.blocking_concurrent"] = 5 # * int(partitions/8)
-        fabric.env["client.count"] = 1#4
+        fabric.env["client.count"] = RECONFIG_CLIENT_COUNT
         fabric.env["client.blocking"] = True
         fabric.env["client.output_response_status"] = True
         fabric.env["client.threads_per_host"] = min(50, int(partitions * 4))
+        fabric.env["site.reconfig_chunk_size_kb"] = 30000
+        fabric.env["site.reconfig_async_chunk_size_kb"] = 30000
 
     if 'stopcopy-2' in args['exp_type']:
         fabric.env["client.blocking_concurrent"] = 5 # * int(partitions/8)
-        fabric.env["client.count"] = 1#4
+        fabric.env["client.count"] = RECONFIG_CLIENT_COUNT
         fabric.env["client.blocking"] = True
         fabric.env["client.output_response_status"] = True
         fabric.env["client.threads_per_host"] = min(50, int(partitions * 4))
+        fabric.env["site.reconfig_chunk_size_kb"] = 30000 
+        fabric.env["site.reconfig_async_chunk_size_kb"] = 30000 
         fabric.env["site.commandlog_enable"] = False
 
     if 'baseline-1' in args['exp_type']:
         fabric.env["client.blocking_concurrent"] = 5 # * int(partitions/8)
-        fabric.env["client.count"] = 1#4
+        fabric.env["client.count"] = RECONFIG_CLIENT_COUNT
         fabric.env["client.blocking"] = True
         fabric.env["client.output_response_status"] = True
         fabric.env["client.threads_per_host"] = min(50, int(partitions * 4))
