32ad4930607e85a57e83fb9b13adb2e91f174c02
Essam Mansour <essam.mansour@gmail.com>
2014-01-09 16:19:37 +0300

    remove CPU checking

--- src/frontend/org/qcri/PartitioningPlanner/Controller.java
+++ src/frontend/org/qcri/PartitioningPlanner/Controller.java
@@ -100,7 +100,7 @@ public class Controller implements Runnable {
 					
 					
 						
-				    ttExecutor.turnOnOff(40);	// turn on tuple tracking for time window of X seconds
+				    ttExecutor.turnOnOff(10);	// turn on tuple tracking for time window of X seconds
 						
 					// here we get top K
 					ttExecutor.getTopKPerPart(no_of_partitions,hotTuplesList);
