2ace8fd4fe9a1a8581743b93286791c5476640ca
Essam Mansour <essam.mansour@gmail.com>
2014-01-13 14:20:41 +0300

    all

--- src/frontend/org/qcri/PartitioningPlanner/Controller.java
+++ src/frontend/org/qcri/PartitioningPlanner/Controller.java
@@ -117,14 +117,14 @@ public class Controller implements Runnable {
 					// hotTuplesList.size()
 					currentPlan = algo.computePlan(hotTuplesList, mSiteLoad, "test.txt", hotTuplesList.size());
 
-					currentPlan.toJSON("test1.txt");
+					currentPlan.toJSON("test.txt");
 
 						if(connectedHost == null){
 						    connectToHost();
 						}
  						ClientResponse cresponse = null;
 						try {
-						    cresponse = client.callProcedure("@Reconfiguration", 0, "test1.txt", "livepull");
+						    cresponse = client.callProcedure("@Reconfiguration", 0, "test.txt", "livepull");
 						    System.out.println("Controller: received response: " + cresponse);
 						} catch (NoConnectionsException e) {
 						    System.out.println("Controller: lost connection");
