39de03ebaac5046f342029fdd8f9ee25a190c1e6
Essam Mansour <essam.mansour@gmail.com>
2014-01-15 11:41:31 +0300

    Essam set parameters for no. of partitions, time window, planner ID

--- src/frontend/org/qcri/PartitioningPlanner/Controller.java
+++ src/frontend/org/qcri/PartitioningPlanner/Controller.java
@@ -305,7 +305,7 @@ public class Controller implements Runnable {
 		
 		//ArgumentsParser args = ArgumentsParser.load(params,ArgumentsParser.PARAM_CATALOG);
 		
-		System.out.println("Params: bench"+params[0] +" no. part " +vargs[1] + " twin "+vargs[2]+" plannerID "+vargs[3]);
+		System.out.println("Params: bench"+params[0] );//+" no. part " +vargs[1] + " twin "+vargs[2]+" plannerID "+vargs[3]);
 		
 		//no_of_partitions = Integer.parseInt(vargs[1]);
 		//time_window = Integer.parseInt(vargs[2]);
