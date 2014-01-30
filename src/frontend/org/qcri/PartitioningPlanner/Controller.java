d96312d101dc67690431b582cb5ff55e9cb7cdcd
Essam Mansour <essam.mansour@gmail.com>
2014-01-30 16:45:33 +0300

    voter tuple tracking Merged with Becca's code

--- src/frontend/org/qcri/PartitioningPlanner/Controller.java
+++ src/frontend/org/qcri/PartitioningPlanner/Controller.java
@@ -172,6 +172,7 @@ public class Controller implements Runnable {
 
 			ttExecutor.turnOnOff(time_window,client);	// turn on tuple tracking for time window of X seconds
 
+			
 			// here we get top K
 			ttExecutor.getTopKPerPart(no_of_partitions,hotTuplesList, client);
 
