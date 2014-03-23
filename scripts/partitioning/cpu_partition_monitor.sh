c507dafe4c3250da69c32584721dec8d84806d4c
marcoserafini <marco.serafini@gmail.com>
2014-03-23 08:59:05 +0300

    some fixes of the controller

--- scripts/partitioning/cpu_partition_monitor.sh
+++ scripts/partitioning/cpu_partition_monitor.sh
@@ -13,4 +13,4 @@ fi
 PID=$(top -n1 | grep -m1 java | perl -pe 's/\e\[?.*?[\@-~] ? ?//g' 2> /dev/null | cut -f1 -d' ')
 #PID=$(top -n1 | grep -m1 java | cut -f1 -d' ')
 NID=$(printf '%d' $(jstack $PID | grep -m1 "H${1}-${2}" | cut -d '=' -f 4 | cut -d ' ' -f 1))
-top -H -n1 | grep -m1 $NID
+top -H -n1 | grep java | grep -m1 $NID
