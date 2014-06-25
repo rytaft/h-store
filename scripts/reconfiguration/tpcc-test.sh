3705360b0e28cc6a98c603ebb58ecc7a7e23ef10
Aaron Elmore <aelmore@csail.mit.edu>
2014-06-25 17:14:22 -0400

    updated scripts

--- scripts/reconfiguration/tpcc-test.sh
+++ scripts/reconfiguration/tpcc-test.sh
@@ -15,13 +15,19 @@ FABRIC_TYPE="ssh"
 FIRST_PARAM_OFFSET=0
 
 EXP_TYPES=( \
-    "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=5000 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-10-size-20-delay-5"\
-    "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=5 --plandelay=5000 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-5-size-20-delay-5"\
-    "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=25 --plandelay=5000 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-25-size-20-delay-5"\
-    "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=50 --plandelay=5000 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-50-size-20-delay-5"\    
-    #"reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=1000 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-10-size-20-delay-1"\
-    #"reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=10000 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-10-size-20-delay-10"\
-    #"reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=500 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-10-size-20-delay-.5"\
+    "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=2000 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-10-size-20-delay-2"\
+    "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=2000 --chunksize=10000 --asyncsize=10000  --exp-suffix=split-10-size-10-delay-2"\
+    "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=2000 --chunksize=5000 --asyncsize=5000  --exp-suffix=split-10-size-5-delay-2"\
+    "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=2000 --chunksize=500 --asyncsize=500  --exp-suffix=split-10-size-.5-delay-2"\
+    "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=2000 --chunksize=1000 --asyncsize=1000  --exp-suffix=split-10-size-1-delay-2"\
+    "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=2000 --chunksize=30000 --asyncsize=2000  --exp-suffix=split-10-size-30l2a-delay-2"\
+    # "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=5000 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-10-size-20-delay-5"\
+    # "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=5 --plandelay=5000 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-5-size-20-delay-5"\
+    # "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=25 --plandelay=5000 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-25-size-20-delay-5"\
+    # "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=50 --plandelay=5000 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-50-size-20-delay-5"\    
+    # "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=1000 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-10-size-20-delay-1"\
+    # "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=10000 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-10-size-20-delay-10"\
+    # "reconfig-dynsplit --partitions=2 --benchmark-size=4 --splitplan=10 --plandelay=500 --chunksize=20000 --asyncsize=20000  --exp-suffix=split-10-size-20-delay-.5"\
 )
 
 for b in tpcc; do
