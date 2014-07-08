f6dfe459634acea80a4b26711444124db31a2ca9
Rebecca Taft <rytaft@mit.edu>
2014-07-07 20:37:44 -0400

    fix compile error

--- tests/frontend/org/qcri/PartitioningPlanner/placement/TestBinPackerPlacement.java
+++ tests/frontend/org/qcri/PartitioningPlanner/placement/TestBinPackerPlacement.java
@@ -88,7 +88,7 @@ public class TestBinPackerPlacement extends BaseTestCase {
 		}
 		
 
-		aPlan = aPlacement.computePlan(hotTuplesList, partitionTotals,  "test.txt", partitionTotals.size(), 60000);
+		aPlan = aPlacement.computePlan(hotTuplesList, partitionTotals,  "test.txt", partitionTotals.size(), 60000, this.getCatalogContext());
 
 		System.out.println("Ending with plan:");
 		aPlan.printPlan();
