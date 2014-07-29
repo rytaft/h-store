b642f3fd7d7f69f026649bb2b4fef8e35e16ff5e
Aaron Elmore <aelmore@csail.mit.edu>
2014-07-29 09:42:13 -0400

    new plan

---
+++ scripts/reconfiguration/tpcc-100.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------------
+
+trap onexit 1 2 3 15
+function onexit() {
+    local exit_status=${1:-$?}
+    exit $exit_status
+}
+
+# ---------------------------------------------------------------------
+
+DATA_DIR="out"
+FABRIC_TYPE="ssh"
+FIRST_PARAM_OFFSET=0
+
+EXP_TYPES=( 
+    "reconfig-dynsplit-fine-grained --partitions=18 --benchmark-size=100 --splitplan=4 --plandelay=500 --chunksize=10048 --asyncsize=8048 --asyncdelay=100 --global.hasher_plan=scripts/reconfiguration/plans/tpcc-size100-18-fine.json" 
+    )
+
+for b in tpcc; do
+    PARAMS=( \
+        --no-update \
+        --results-dir=$DATA_DIR \
+        --benchmark=$b \
+        --stop-on-error \
+        --exp-trials=1 \
+        --exp-attempts=1 \        
+        --no-json \
+        --plot \
+	    --client.interval=1000 \
+        --client.output_interval=true \
+        --client.duration=300000 \
+        --client.warmup=10000 \
+        --client.output_results_csv=interval_res.csv \
+        --reconfig=60000:1:0 \
+        --sweep-reconfiguration 
+    )
+   
+    
+    i=0
+    cnt=${#EXP_TYPES[@]}
+    while [ "$i" -lt "$cnt" ]; do
+        ./experiment-runner.py $FABRIC_TYPE ${PARAMS[@]:$FIRST_PARAM_OFFSET} \
+            --exp-type=${EXP_TYPES[$i]}
+        FIRST_PARAM_OFFSET=0
+        i=`expr $i + 1`
+    done
+
+done
