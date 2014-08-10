ee026e5c7e7d66465f71e34bc5e7ad50b8cc2f32
Aaron Elmore <aelmore@csail.mit.edu>
2014-08-10 13:20:46 -0400

    test plan

---
+++ scripts/reconfiguration/tpcc-20.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------------
+
+trap onexit 1 2 3 15
+function onexit() {
+    local exit_status=${1:-$?}
+    exit $exit_status
+}
+
+# ---------------------------------------------------------------------
+
+DATA_DIR="out"
+FABRIC_TYPE="ssh"
+FIRST_PARAM_OFFSET=0
+
+EXP_TYPES=( 
+    "reconfig-dynsplit --partitions=16 --benchmark-size=20 --exp-suffix=s2 --splitplan=2 --plandelay=100 --asyncdelay=50 " 
+    #"reconfig-dynsplit-fine-grained --partitions=18 --benchmark-size=100 --exp-suffix=s20 --splitplan=20 --plandelay=100 --asyncdelay=50 --global.hasher_plan=scripts/reconfiguration/plans/tpcc-size100-18-fine.json" 
+    #"reconfig-dynsplit-fine-grained --partitions=18 --benchmark-size=100 --exp-suffix=s4 --splitplan=4 --plandelay=100 --asyncdelay=50 --global.hasher_plan=scripts/reconfiguration/plans/tpcc-size100-18-fine.json" 
+    #"reconfig-dynsplit-fine-grained --partitions=18 --benchmark-size=100  --plandelay=100 --asyncdelay=50 --global.hasher_plan=scripts/reconfiguration/plans/tpcc-size100-18-fine.json" 
+    #"reconfig-dynsplit --partitions=18 --benchmark-size=100 --plandelay=100 --asyncdelay=50 " 
+    #"stopcopy-dynsplit --partitions=18 --benchmark-size=100  --plandelay=100 --asyncdelay=50 " 
+    #"reactive -dynsplit --partitions=18 --benchmark-size=100 --plandelay=100 --asyncdelay=50 " 
+    )
+
+for b in tpcc; do
+    PARAMS=( \
+        --no-update \
+        --results-dir=$DATA_DIR \
+        --benchmark=$b \
+        --stop-on-error \
+        --exp-trials=1 \
+        --exp-attempts=1 \        
+        --no-json \
+        --plot \
+	    --client.interval=1000 \
+        --client.output_interval=true \
+        --client.duration=300000 \
+        --client.warmup=10000 \
+        --client.output_results_csv=interval_res.csv \
+        --reconfig=60000:1:0 \
+        --sweep-reconfiguration 
+    )
+   
+    
+    i=0
+    cnt=${#EXP_TYPES[@]}
+    while [ "$i" -lt "$cnt" ]; do
+        ./experiment-runner.py $FABRIC_TYPE ${PARAMS[@]:$FIRST_PARAM_OFFSET} \
+            --exp-type=${EXP_TYPES[$i]}
+        FIRST_PARAM_OFFSET=0
+        i=`expr $i + 1`
+    done
+
+done
