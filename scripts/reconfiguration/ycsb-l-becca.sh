0aacb5a64e3e70e436f886dced3a38f577a123f0
Aaron Elmore <aelmore@csail.mit.edu>
2014-11-10 23:17:08 -0500

    moar optimizations tests

--- scripts/reconfiguration/ycsb-l-becca.sh
+++ scripts/reconfiguration/ycsb-l-becca.sh
@@ -15,21 +15,43 @@ FABRIC_TYPE="ssh"
 FIRST_PARAM_OFFSET=0
 
 EXP_TYPES=( 
-    "nonopt-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2 --reconfig=30000:spread2:0 " 
-    "split-merge-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2 --reconfig=30000:spread2:0 " 
-    "eager-pull-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2 --reconfig=30000:spread2:0 " 
-    "reconfig-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2 --reconfig=30000:spread2:0 " 
-    "reconfig-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2_s5 --splitplan=5 --reconfig=30000:spread2:0 "
+    #"nonopt-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2 --reconfig=30000:spread2:0 " 
+    #"split-merge-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2 --reconfig=30000:spread2:0 " 
+    #"eager-pull-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2 --reconfig=30000:spread2:0 " 
+    #"reconfig-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2 --reconfig=30000:spread2:0 " 
+    #"reconfig-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2_s5 --splitplan=5 --reconfig=30000:spread2:0 "
 
-    "nonopt-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=lc2 --reconfig=30000:contract2:0 " 
-    "split-merge-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=lc2 --reconfig=30000:contract2:0 " 
-    "eager-pull-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=lc2 --reconfig=30000:contract2:0 " 
-    "reconfig-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=lc2 --reconfig=30000:contract2:0 " 
-    "reconfig-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=lc2_s5 --splitplan=5  --reconfig=30000:contract2:0 " 
+    #"nonopt-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=lc2 --reconfig=30000:contract2:0 " 
+    #"split-merge-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=lc2 --reconfig=30000:contract2:0 " 
+    #"eager-pull-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=lc2 --reconfig=30000:contract2:0 " 
+    #"reconfig-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=lc2 --reconfig=30000:contract2:0 " 
+    #"reconfig-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=lc2_s5 --splitplan=5  --reconfig=30000:contract2:0 " 
 
-    "nonopt-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2_s5 --splitplan=5 --reconfig=30000:spread2:0 "
-    "split-merge-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2_s5 --splitplan=5 --reconfig=30000:spread2:0 "
-    "eager-pull-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2_s5 --splitplan=5 --reconfig=30000:spread2:0 "
+    #"nonopt-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2_s5 --splitplan=5 --reconfig=30000:spread2:0 "
+    #"split-merge-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2_s5 --splitplan=5 --reconfig=30000:spread2:0 "
+    #"eager-pull-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=ls2_s5 --splitplan=5 --reconfig=30000:spread2:0 "
+    
+    "nonopt-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=lc2_s5 --splitplan=5 --reconfig=30000:contract2:0 " 
+    "split-merge-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=lc2_s5 --splitplan=5 --reconfig=30000:contract2:0 " 
+    "eager-pull-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=lc2_s5 --splitplan=5 --reconfig=30000:contract2:0 " 
+
+    "nonopt-ycsb-uniform --partitions=16 --benchmark-size=10000000 --exp-suffix=shuf --reconfig=30000:shuffle:0 " 
+    "split-merge-only--ycsb-uniform --partitions=16 --benchmark-size=10000000 --exp-suffix=shuf --reconfig=30000:shuffle:0 " 
+    "eager-pull-only-ycsb-uniform --partitions=16 --benchmark-size=10000000 --exp-suffix=shuf --reconfig=30000:shuffle:0 " 
+    "reconfig-ycsb-uniform --partitions=16 --benchmark-size=10000000 --exp-suffix=shuf  --reconfig=30000:shuffle:0 "
+    "reconfig-ycsb-uniform --partitions=16 --benchmark-size=10000000 --exp-suffix=shuf_s5 --splitplan=5  --reconfig=30000:shuffle:0 "
+    "split-merge-only--ycsb-uniform --partitions=16 --benchmark-size=10000000 --exp-suffix=shuf_s5 --splitplan=5  --reconfig=30000:shuffle:0 " 
+    "eager-pull-only-ycsb-uniform --partitions=16 --benchmark-size=10000000 --exp-suffix=shuf_s5 --splitplan=5  --reconfig=30000:shuffle:0 "  
+  
+    "nonopt-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=exp --reconfig=30000:expand:0 --global.hasher_plan=scripts/reconfiguration/plans/y-l-expand.json" 
+    "split-merge-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=exp --reconfig=30000:expand:0 --global.hasher_plan=scripts/reconfiguration/plans/y-l-expand.json" 
+    "eager-pull-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=exp --reconfig=30000:expand:0 --global.hasher_plan=scripts/reconfiguration/plans/y-l-expand.json" 
+    "reconfig-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=exp --reconfig=30000:expand:0 --global.hasher_plan=scripts/reconfiguration/plans/y-l-expand.json" 
+    "reconfig-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=exp_s5 --splitplan=5  --reconfig=30000:expand:0 --global.hasher_plan=scripts/reconfiguration/plans/y-l-expand.json" 
+    "split-merge-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=exp_s5 --splitplan=5   --reconfig=30000:expand:0 --global.hasher_plan=scripts/reconfiguration/plans/y-l-expand.json" 
+    "eager-pull-only-ycsb-zipf --partitions=16 --benchmark-size=10000000 --exp-suffix=exp_s5 --splitplan=5   --reconfig=30000:expand:0 --global.hasher_plan=scripts/reconfiguration/plans/y-l-expand.json"   
+    
+
 )
 
 
