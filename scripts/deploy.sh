70276e543be0e4573532f9e20e27a44b9622ea69
marcoserafini <marco.serafini@gmail.com>
2015-01-28 07:21:22 +0300

    configuration parameters for experiments

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,38 +2,37 @@
 
 for s in da01 da02 da04 da05 da06 da07 da08 da09 da10 da11 da12 da14 da15
 do
+	echo "copying to $s"
         if [[ "$s" != "$HOSTNAME" ]]
         then
-		scp tpcc-plan-*.pplan $s:/localdisk/mserafini/h-store
+		scp *.json $s:/localdisk/mserafini/h-store > /dev/null
 		if [[ $? -ne 0 ]]
 		then
-			echo "Failed copying to $s"
+			echo "Failed copying json plans $s"
+			echo "$?"
 			exit 1
 		fi 
-		scp *.json $s:/localdisk/mserafini/h-store
+		scp *.jar $s:/localdisk/mserafini/h-store > /dev/null
 		if [[ $? -ne 0 ]]
 		then
-			echo "Failed copying to $s"
-			exit 1
-		fi 
-		scp *.jar $s:/localdisk/mserafini/h-store
-		if [[ $? -ne 0 ]]
-		then
-			echo "Failed copying to $s"
+			echo "Failed copying jar files $s"
+			echo "$?"
 			exit 1
 		fi
 		if [[ "$1" != "config" ]]  
 		then
-			rm -rf $s:/localdisk/mserafini/h-store/obj/release/*
+			rm -rf $s:/localdisk/mserafini/h-store/obj/release/* > /dev/null
 			if [[ $? -ne 0 ]]
 			then
-				echo "Failed copying to $s"
+				echo "Failed removing executables from $s"
+				echo "$?"
 				exit 1
 			fi 
-			scp -r obj/release/* $s:/localdisk/mserafini/h-store/obj/release
+			scp -r obj/release/* $s:/localdisk/mserafini/h-store/obj/release > /dev/null
 			if [[ $? -ne 0 ]]
 			then
-				echo "Failed copying to $s"
+				echo "Failed copying executables to $s"
+				echo "$?"
 				exit 1
 			fi 
 		fi
