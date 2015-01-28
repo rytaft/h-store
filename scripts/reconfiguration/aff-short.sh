aa8d0da38b8fe58613e15f24f65fccde968c5ae4
Aaron Elmore <aelmore@csail.mit.edu>
2015-01-27 22:50:05 -0500

    small test

---
+++ scripts/reconfiguration/aff-short.sh
@@ -0,0 +1,50 @@
+confi#!/bin/bash
+
+# ---------------------------------------------------------------------
+
+trap onexit 1 2 3 15
+function onexit() {
+    local exit_status=${1:-$?}
+    exit $exit_status
+}
+
+# ---------------------------------------------------------------------
+
+DATA_DIR="out"
+FABRIC_TYPE="ssh"
+FIRST_PARAM_OFFSET=0
+
+EXP_TYPES=( \
+    "affinity-dyn --partitions=4 --benchmark-size=xs --exp-suffix=xs" \
+)
+
+for b in affinity; do
+    PARAMS=( \
+        --no-update \
+        --results-dir=$DATA_DIR \
+        --benchmark=$b \
+        --stop-on-error \
+        --exp-trials=1 \
+        --exp-attempts=1 \        
+        --no-json \
+        --plot \
+	    --client.interval=1000 \
+        --client.output_interval=true \
+        --client.duration=120000 \
+        --client.warmup=10000 \
+        --client.output_results_csv=interval_res.csv \
+        --affinity=10000 \
+        --sweep-reconfiguration 
+    )
+        #--global.hasher_plan=plan_affinity.json \
+    
+    i=0
+    cnt=${#EXP_TYPES[@]}
+    while [ "$i" -lt "$cnt" ]; do
+        ./experiment-runner.py $FABRIC_TYPE ${PARAMS[@]:$FIRST_PARAM_OFFSET} \
+            --exp-type=${EXP_TYPES[$i]}
+        FIRST_PARAM_OFFSET=0
+        i=`expr $i + 1`
+    done
+
+done
