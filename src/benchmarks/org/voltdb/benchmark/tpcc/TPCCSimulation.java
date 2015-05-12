912523c4ff6eafc438ba5fc9cb4ef45a0a1f0a16
Marco Serafini <marco.serafini@gmail.com>
2015-05-12 20:22:17 +0300

    minor fix

--- src/benchmarks/org/voltdb/benchmark/tpcc/TPCCSimulation.java
+++ src/benchmarks/org/voltdb/benchmark/tpcc/TPCCSimulation.java
@@ -316,7 +316,7 @@ public class TPCCSimulation {
         
         int remote_w_id = 0;
         
-        if(generator.number(1, 100) < config.warehouse_pairing_pct){
+        if(generator.number(1, 100) < config.warehouse_pairing_percent){
             remote_w_id = (w_id % 2 == 0 ? w_id+1 : w_id-1);
             if (remote_w_id < starting_warehouse) remote_w_id = last_warehouse;
             else if (remote_w_id > last_warehouse) remote_w_id = starting_warehouse;
