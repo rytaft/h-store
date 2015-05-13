dc9b784071face3e2bb318d03328620a8ab3270c
Marco Serafini <marco.serafini@gmail.com>
2015-05-13 16:35:35 +0300

    fixed numbering of warehouses in TPC-c

--- src/benchmarks/org/voltdb/benchmark/tpcc/TPCCSimulation.java
+++ src/benchmarks/org/voltdb/benchmark/tpcc/TPCCSimulation.java
@@ -318,7 +318,8 @@ public class TPCCSimulation {
         int remote_w_id = 0;
         
         if(generator.number(1, 100) < warehouse_pairing_percent){
-            remote_w_id = (w_id % 2 == 0 ? w_id+1 : w_id-1);
+            // in TPC-C partitions are numbered from 1
+            remote_w_id = (w_id % 2 == 0 ? w_id-1 : w_id+1);
             if (remote_w_id < starting_warehouse) remote_w_id = last_warehouse;
             else if (remote_w_id > last_warehouse) remote_w_id = starting_warehouse;
         }
