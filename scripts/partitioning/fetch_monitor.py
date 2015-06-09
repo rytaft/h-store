b75c45a773bc7dbe13573256f00e28130ded2e0f
Marco Serafini <marco.serafini@gmail.com>
2015-06-09 09:01:48 +0300

    fixed script to fecth monitoring files

--- scripts/partitioning/fetch_monitor.py
+++ scripts/partitioning/fetch_monitor.py
@@ -9,6 +9,6 @@ if not ('localhost' in sys.argv[2:]):
 
 procs = []
 for ip in sys.argv[2:]:
-	p = subprocess.Popen('ssh ' + ip + ' \"tar -zcf ' + dir + '/monitoring-' + ip + '.tar.gz ' + dir + '/transactions-partition* -P" ; scp -q ' + ip + ':' + dir + '/monitoring-' + ip + '.tar.gz ' + dir + '/. ; tar -xf ' + dir + '/monitoring-' + ip + '.tar.gz -C / -P', shell=True)
+	p = subprocess.Popen('ssh ' + ip + ' \"tar -zcf ' + dir + '/monitoring-' + ip + '.tar.gz ' + dir + '/*-partition-*.log -P" ; scp -q ' + ip + ':' + dir + '/monitoring-' + ip + '.tar.gz ' + dir + '/. ; tar -xf ' + dir + '/monitoring-' + ip + '.tar.gz -C / -P', shell=True)
 	procs.append(p)
 [p.wait() for p in procs]
