20c01e6c1d7c7d85aa21901f847cd1d6e3c2b0f7
Marco Serafini <marco.serafini@gmail.com>
2015-11-08 02:59:35 -0500

    changes in configs

--- scripts/save_results.sh
+++ scripts/save_results.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 mkdir $1
-cp *.json $1
-cp *-partition*.log $1
-cp -r obj/logs $1
-cp *.csv $1
+cp plan_out.json $1
+cp -r obj/logs $1-logs
+cp results.csv $1
 cp out.log $1
 cp hevent.log $1
