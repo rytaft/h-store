cddd7589c5a1286ec63fcf3e27c5d278fce9b7e3
Marco Serafini <marco.serafini@gmail.com>
2015-11-13 02:03:17 -0500

    config files

---
+++ configs/affinity-50GB-hotsupplier/affinity-controller.sh
@@ -0,0 +1,1 @@
+ant affinity -Dproject=affinity -Dglobal.hasher_plan=plan.json -Dglobal.hasher_class=edu.brown.hashing.TwoTieredRangeHasher -Delastic.run_monitoring=false -Delastic.update_plan=true -Delastic.exec_reconf=false -Delastic.max_load=4000 -Delastic.algo=graph -Dclient.memory=4096 -Delastic.max_partitions_added=6 | tee out.log
