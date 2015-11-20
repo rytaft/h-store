7e276d00318f54e2ee18c2d5e011e2eda97621ef
Marco Serafini <marco.serafini@gmail.com>
2015-11-20 03:22:28 +0300

    fix

--- src/frontend/org/qcri/affinityplanner/GraphGreedy.java
+++ src/frontend/org/qcri/affinityplanner/GraphGreedy.java
@@ -480,13 +480,12 @@ public class GraphGreedy extends PartitionerAffinity {
 
                         vertexSingleton.add(adjacentVertex);
                         // setting the destination partition to be remote = worst case for the sender
-                        double newVertexPartitionDelta = getSenderDelta(vertexSingleton, newVertexPartition, false);
                         vertexSingleton.remove(adjacentVertex);
 
                         if (affinity > maxAffinity
                                 && !vertices.contains(adjacentVertex)
                                 && newVertexPartition != senderPartition 
-                                && newVertexPartitionDelta <= 0) {
+                                && getSenderDelta(vertexSingleton, newVertexPartition, false) <= 0) {
                             maxAffinity = affinity;
                             res = adjacentVertex;
 
