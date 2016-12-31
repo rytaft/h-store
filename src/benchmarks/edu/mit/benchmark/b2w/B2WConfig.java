f0635f778320129fdd79adba410ceba7099144be
Rebecca Taft <rytaft@mit.edu>
2016-12-31 09:21:11 -0500

    fix b2w config

--- src/benchmarks/edu/mit/benchmark/b2w/B2WConfig.java
+++ src/benchmarks/edu/mit/benchmark/b2w/B2WConfig.java
@@ -16,13 +16,7 @@ public class B2WConfig {
         LoggerUtil.attachObserver(LOG, debug);
     }
      
-    public Random rand_gen;
     public Integer loadthreads = ThreadUtil.availableProcessors();
-    public String cart_data_file = null;
-    public String checkout_data_file = null;
-    public String stock_transaction_data_file = null;
-    public String stock_quantity_data_file = null;
-    public String stock_inventory_data_file = null;
     public String operations_file = null;
 
     public String STK_INVENTORY_STOCK_DATA_FILE = null;
@@ -46,29 +40,12 @@ public class B2WConfig {
 
     
     public B2WConfig(Map<String, String> m_extraParams) {
-        this.rand_gen = new Random(); 
-        
         for (String key : m_extraParams.keySet()) {
             String value = m_extraParams.get(key);
 
             // Multi-Threaded Loader
             if (key.equalsIgnoreCase("loadthreads")) {
                 this.loadthreads  = Integer.valueOf(value);
-            }
-            else if (key.equalsIgnoreCase("cart_data_file")) {
-                this.cart_data_file = String.valueOf(value);
-            }
-            else if (key.equalsIgnoreCase("checkout_data_file")) {
-                this.checkout_data_file = String.valueOf(value);
-            }
-            else if (key.equalsIgnoreCase("stock_transaction_data_file")) {
-                this.stock_transaction_data_file = String.valueOf(value);
-            }
-            else if (key.equalsIgnoreCase("stock_quantity_data_file")) {
-                this.stock_quantity_data_file = String.valueOf(value);
-            }
-            else if (key.equalsIgnoreCase("stock_inventory_data_file")) {
-                this.stock_inventory_data_file = String.valueOf(value);
             }
             else if (key.equalsIgnoreCase("operations_file")) {
                 this.operations_file = String.valueOf(value);
@@ -126,16 +103,32 @@ public class B2WConfig {
             }
         } // FOR
             
+        LOG.info(this.toString());
     }
 
     
     
     @Override
     public String toString() {
-        return "B2WConfig [loadthreads=" + loadthreads + ", cart_data_file=" + cart_data_file
-                + ", checkout_data_file=" + checkout_data_file + ", stock_transaction_data_file=" + stock_transaction_data_file
-                + ", stock_quantity_data_file=" + stock_quantity_data_file + ", stock_inventory_data_file=" + stock_inventory_data_file
-                + ", operations_file=" + operations_file + "]";
+        return "B2WConfig [loadthreads=" + loadthreads
+                + ", operations_file=" + operations_file 
+                + ", STK_INVENTORY_STOCK_DATA_FILE=" + STK_INVENTORY_STOCK_DATA_FILE
+                + ", STK_INVENTORY_STOCK_QUANTITY_DATA_FILE=" + STK_INVENTORY_STOCK_QUANTITY_DATA_FILE
+                + ", STK_STOCK_TRANSACTION_DATA_FILE=" + STK_STOCK_TRANSACTION_DATA_FILE
+                + ", CART_DATA_FILE=" + CART_DATA_FILE
+                + ", CART_CUSTOMER_DATA_FILE=" + CART_CUSTOMER_DATA_FILE
+                + ", CART_LINES_DATA_FILE=" + CART_LINES_DATA_FILE
+                + ", CART_LINE_PRODUCTS_DATA_FILE=" + CART_LINE_PRODUCTS_DATA_FILE
+                + ", CART_LINE_PROMOTIONS_DATA_FILE=" + CART_LINE_PROMOTIONS_DATA_FILE
+                + ", CART_LINE_PRODUCT_WARRANTIES_DATA_FILE=" + CART_LINE_PRODUCT_WARRANTIES_DATA_FILE
+                + ", CART_LINE_PRODUCT_STORES_DATA_FILE=" + CART_LINE_PRODUCT_STORES_DATA_FILE
+                + ", CHECKOUT_DATA_FILE=" + CHECKOUT_DATA_FILE
+                + ", CHECKOUT_PAYMENTS_DATA_FILE=" + CHECKOUT_PAYMENTS_DATA_FILE
+                + ", CHECKOUT_FREIGHT_DELIVERY_TIME_DATA_FILE=" + CHECKOUT_FREIGHT_DELIVERY_TIME_DATA_FILE
+                + ", CHECKOUT_STOCK_TRANSACTIONS_DATA_FILE=" + CHECKOUT_STOCK_TRANSACTIONS_DATA_FILE
+                + ", speed_up=" + speed_up
+                + ", sleep_time=" + sleep_time
+                + ", start_offset=" + start_offset + "]";
     }
 
 }
