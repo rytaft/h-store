6142a41a721719ca8ad3a6db225744df3f1cc0f6
Rebecca Taft <rytaft@mit.edu>
2017-02-09 16:05:41 -0500

    more complex tests

--- tests/frontend/org/qcri/affinityplanner/TestReconfigurationPredictor.java
+++ tests/frontend/org/qcri/affinityplanner/TestReconfigurationPredictor.java
@@ -12,27 +12,29 @@ import edu.brown.utils.ProjectType;
  */
 public class TestReconfigurationPredictor extends BaseTestCase {
     
-    private double[] load_predictions_arr = new double[]{ 
+    private double[] load_predictions_arr_1 = new double[]{ 
             100, 200, 300, 400, 500, 600, 600, 500, 400, 300, 200, 100, 
             100, 200, 300, 400, 500, 600, 600, 500, 400, 300, 200, 100 };
-    private double capacity_per_node = 101;
-    private int nodes_start = 1;
-    private int db_migration_time = 2;
-    ReconfigurationPredictor predictor;
+    private double capacity_per_node_1 = 100;
+    private int nodes_start_1 = 1;
+    private int db_migration_time_1 = 2;
+    
+    private double[] load_predictions_arr_2 = new double[]{ 
+            600, 500, 400, 300, 200, 100, 100, 200, 300, 400, 500, 600, 
+            600, 500, 400, 300, 200, 100, 100, 200, 300, 400, 500, 600 };
+    private double capacity_per_node_2 = 100;
+    private int nodes_start_2 = 6;
+    private int db_migration_time_2 = 2;
     
     @Override
     protected void setUp() throws Exception {
         super.setUp(ProjectType.B2W);
-        ArrayList<Double> load_predictions = new ArrayList<>();
-        for (double prediction : load_predictions_arr) load_predictions.add(prediction);
-        predictor = new ReconfigurationPredictor(capacity_per_node, 
-                load_predictions, nodes_start, db_migration_time);
     }
     
-    public void testBestMoves() throws Exception {
-        ArrayList<Move> moves = predictor.bestMoves();
+    private void checkCorrect(ReconfigurationPredictor predictor, ArrayList<Move> moves, 
+            double[] load_predictions_arr, boolean print) {
         Move prev_move = null;
-        System.out.println("   Capacity \tLoad");
+        if (print) System.out.println("   Capacity \tLoad");
         for (Move move : moves) {
             assertTrue(predictor.capacity(move.nodes) >= load_predictions_arr[move.time]);
             if (prev_move != null) {
@@ -40,13 +42,134 @@ public class TestReconfigurationPredictor extends BaseTestCase {
                 for (int i = 1; i < move.time - prev_move.time; ++i) {
                     double effectiveCap = predictor.effectiveCapacity(i, reconfig_time, prev_move.nodes, move.nodes);
                     assertTrue(effectiveCap >= load_predictions_arr[prev_move.time+i]);
-                    System.out.println(prev_move.time+i + ": " + effectiveCap + " \t" + load_predictions_arr[prev_move.time+i]);
+                    if (print) System.out.println(prev_move.time+i + ": " + effectiveCap + " \t" + load_predictions_arr[prev_move.time+i]);
                 }
             }
-            System.out.println(move.time + ": " + predictor.capacity(move.nodes) + " \t" + load_predictions_arr[move.time]);
+            if (print) System.out.println(move.time + ": " + predictor.capacity(move.nodes) + " \t" + load_predictions_arr[move.time]);
             prev_move = move;
             
         }
+    }
+    
+    public void testBestMoves1() throws Exception {
+        ArrayList<Double> load_predictions = new ArrayList<>();
+        for (double prediction : load_predictions_arr_1) load_predictions.add(prediction);
+
+        // defaults
+        ReconfigurationPredictor predictor = new ReconfigurationPredictor(capacity_per_node_1, 
+                load_predictions, nodes_start_1, db_migration_time_1);        
+        ArrayList<Move> moves = predictor.bestMoves();
+        checkCorrect(predictor, moves, load_predictions_arr_1, true);
+        
+        // increased capacity
+        predictor = new ReconfigurationPredictor(capacity_per_node_1 + 10, 
+                load_predictions, nodes_start_1, db_migration_time_1);
+        moves = predictor.bestMoves();
+        checkCorrect(predictor, moves, load_predictions_arr_1, true);
+        
+        // increased starting nodes
+        predictor = new ReconfigurationPredictor(capacity_per_node_1, 
+                load_predictions, nodes_start_1 + 1, db_migration_time_1);
+        moves = predictor.bestMoves();
+        checkCorrect(predictor, moves, load_predictions_arr_1, true);
+        
+        // increased time to move data
+        predictor = new ReconfigurationPredictor(capacity_per_node_1, 
+                load_predictions, nodes_start_1, 3);
+        moves = predictor.bestMoves();
+        assertTrue(moves == null);
+    }
+    
+    public void testBestMoves2() throws Exception {
+        ArrayList<Double> load_predictions = new ArrayList<>();
+        for (double prediction : load_predictions_arr_2) load_predictions.add(prediction);
+
+        // defaults
+        ReconfigurationPredictor predictor = new ReconfigurationPredictor(capacity_per_node_2, 
+                load_predictions, nodes_start_2, db_migration_time_2);        
+        ArrayList<Move> moves = predictor.bestMoves();
+        checkCorrect(predictor, moves, load_predictions_arr_2, true);
+        
+        // increased capacity
+        predictor = new ReconfigurationPredictor(capacity_per_node_2 + 10, 
+                load_predictions, nodes_start_2, db_migration_time_2);
+        moves = predictor.bestMoves();
+        checkCorrect(predictor, moves, load_predictions_arr_2, true);
+        
+        // increased starting nodes
+        predictor = new ReconfigurationPredictor(capacity_per_node_2, 
+                load_predictions, nodes_start_2 + 1, db_migration_time_2);
+        moves = predictor.bestMoves();
+        checkCorrect(predictor, moves, load_predictions_arr_2, true);
+        
+        // increased time to move data
+        predictor = new ReconfigurationPredictor(capacity_per_node_2, 
+                load_predictions, nodes_start_2, 3);
+        moves = predictor.bestMoves();
+        assertTrue(moves == null);
+    }
+    
+    public void testBestMoves3() throws Exception {
+        ArrayList<Double> load_predictions = new ArrayList<>();
+        double load = 100;
+        // first hump
+        for (int i = 0; i < 100; ++i) {
+            load_predictions.add(load);
+            load += 50;
+        }
+        for (int i = 0; i < 100; ++i) {
+            load_predictions.add(load);
+            load += 10;
+        }
+        for (int i = 0; i < 200; ++i) {
+            load_predictions.add(load);
+        }
+        for (int i = 0; i < 100; ++i) {
+            load_predictions.add(load);
+            load -= 10;
+        }
+        for (int i = 0; i < 100; ++i) {
+            load_predictions.add(load);
+            load -= 50;
+        }
+        
+        // second hump
+        for (int i = 0; i < 100; ++i) {
+            load_predictions.add(load);
+            load += 50;
+        }
+        for (int i = 0; i < 100; ++i) {
+            load_predictions.add(load);
+            load += 10;
+        }
+        for (int i = 0; i < 200; ++i) {
+            load_predictions.add(load);
+        }
+        for (int i = 0; i < 100; ++i) {
+            load_predictions.add(load);
+            load -= 10;
+        }
+        for (int i = 0; i < 100; ++i) {
+            load_predictions.add(load);
+            load -= 50;
+        }
+        
+        // spike
+        for (int i = 0; i < 50; ++i) {
+            load_predictions.add(load);
+            load += 200;
+        }
+        for (int i = 0; i < 50; ++i) {
+            load_predictions.add(load);
+            load -= 200;
+        }
+        
+        ReconfigurationPredictor predictor = new ReconfigurationPredictor(100, load_predictions, 2, 2);
+        
+        ArrayList<Move> moves = predictor.bestMoves();
+        double load_predictions_arr[] = new double[load_predictions.size()];
+        for (int i = 0; i < load_predictions_arr.length; ++i) load_predictions_arr[i] = load_predictions.get(i);
+        checkCorrect(predictor, moves, load_predictions_arr, true);
     }
     
 }
