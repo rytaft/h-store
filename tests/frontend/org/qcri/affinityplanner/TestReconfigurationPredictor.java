f17c4b1d66db176ccf0ef3104b5aafb89cbb8bf3
Rebecca Taft <rytaft@mit.edu>
2017-02-13 12:53:35 -0500

    new unit test

--- tests/frontend/org/qcri/affinityplanner/TestReconfigurationPredictor.java
+++ tests/frontend/org/qcri/affinityplanner/TestReconfigurationPredictor.java
@@ -25,6 +25,13 @@ public class TestReconfigurationPredictor extends BaseTestCase {
     private double capacity_per_node_2 = 100;
     private int nodes_start_2 = 6;
     private int db_migration_time_2 = 2;
+    
+    private double[] load_predictions_arr_3 = new double[]{ 
+            100, 100, 100, 100, 100, 600, 100, 100, 100, 100, 100, 100, 
+            100, 100, 100, 100, 100, 400, 100, 100, 100, 100, 100, 100 };
+    private double capacity_per_node_3 = 100;
+    private int nodes_start_3 = 1;
+    private int db_migration_time_3 = 2;
 
     @Override
     protected void setUp() throws Exception {
@@ -108,8 +115,37 @@ public class TestReconfigurationPredictor extends BaseTestCase {
         moves = predictor.bestMoves();
         checkCorrect(predictor, moves, load_predictions_arr_2, true);
     }
-
+    
     public void testBestMoves3() throws Exception {
+        ArrayList<Double> load_predictions = new ArrayList<>();
+        for (double prediction : load_predictions_arr_3) load_predictions.add(prediction);
+
+        // defaults
+        ReconfigurationPredictor predictor = new ReconfigurationPredictor(capacity_per_node_3, 
+                load_predictions, nodes_start_3, db_migration_time_3);        
+        ArrayList<Move> moves = predictor.bestMoves();
+        checkCorrect(predictor, moves, load_predictions_arr_3, true);
+
+        // increased capacity
+        predictor = new ReconfigurationPredictor(capacity_per_node_3 + 10, 
+                load_predictions, nodes_start_3, db_migration_time_3);
+        moves = predictor.bestMoves();
+        checkCorrect(predictor, moves, load_predictions_arr_3, true);
+
+        // increased starting nodes
+        predictor = new ReconfigurationPredictor(capacity_per_node_3, 
+                load_predictions, nodes_start_3 + 1, db_migration_time_3);
+        moves = predictor.bestMoves();
+        checkCorrect(predictor, moves, load_predictions_arr_3, true);
+
+        // increased time to move data
+        predictor = new ReconfigurationPredictor(capacity_per_node_3, 
+                load_predictions, nodes_start_3, 6);
+        moves = predictor.bestMoves();
+        assertTrue(moves == null);
+    }
+
+    public void testBestMoves4() throws Exception {
         ArrayList<Double> load_predictions = new ArrayList<>();
         double load = 100;
         // first hump
