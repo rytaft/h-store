d7e03277c61d0cf74131d5d4d42628a54f5145dd
Rebecca Taft <rytaft@mit.edu>
2017-02-23 07:55:33 -0500

    fix bug

--- src/frontend/org/qcri/affinityplanner/SingleThreadedMigration.java
+++ src/frontend/org/qcri/affinityplanner/SingleThreadedMigration.java
@@ -28,6 +28,9 @@ public class SingleThreadedMigration implements Migration {
     
     // The cost of a move from nodes_before to nodes_after
     public double moveCost(int reconfig_time, int nodes_before, int nodes_after) {
+        if (nodes_before == nodes_after) {
+            return reconfig_time * nodes_before;
+        }
         return reconfig_time * (nodes_after + nodes_before + 1)/2.0;
     }   
 }
