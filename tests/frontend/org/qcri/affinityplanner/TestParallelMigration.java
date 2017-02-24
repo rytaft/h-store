e0118a3cda7d54927dc29d58cc2de22a3fe37ebd
Rebecca Taft <rytaft@mit.edu>
2017-02-24 10:42:26 -0500

    fix test for floating point error

--- tests/frontend/org/qcri/affinityplanner/TestParallelMigration.java
+++ tests/frontend/org/qcri/affinityplanner/TestParallelMigration.java
@@ -16,27 +16,27 @@ public class TestParallelMigration extends BaseTestCase {
     }
 
     public void testCase1() throws Exception {
-        Migration migration = new ParallelMigration(PARTITIONS_PER_SITE, 100);
+        Migration migration = new ParallelMigration(PARTITIONS_PER_SITE, 99);
         assertEquals(2, migration.reconfigTime(3, 5));
-        assertEquals(10, migration.moveCost(2, 3, 5));
+        assertEquals(10, (int) Math.ceil(migration.moveCost(2, 3, 5)));
         assertEquals(2, migration.reconfigTime(5, 3));
-        assertEquals(10, migration.moveCost(2, 5, 3));
+        assertEquals(10, (int) Math.ceil(migration.moveCost(2, 5, 3)));
     }
     
     public void testCase2() throws Exception {
-        Migration migration = new ParallelMigration(PARTITIONS_PER_SITE, 90);
+        Migration migration = new ParallelMigration(PARTITIONS_PER_SITE, 89);
         assertEquals(2, migration.reconfigTime(3, 9));
-        assertEquals(15, migration.moveCost(2, 3, 9));
+        assertEquals(15, (int) Math.ceil(migration.moveCost(2, 3, 9)));
         assertEquals(2, migration.reconfigTime(9, 3));
-        assertEquals(15, migration.moveCost(2, 9, 3));
+        assertEquals(15, (int) Math.ceil(migration.moveCost(2, 9, 3)));
     }
     
     public void testCase3() throws Exception {
-        Migration migration = new ParallelMigration(PARTITIONS_PER_SITE, 30 * 14);
+        Migration migration = new ParallelMigration(PARTITIONS_PER_SITE, 30 * 14 - 1);
         assertEquals(11, migration.reconfigTime(3, 14));
-        assertEquals(3*6 + 3*9 + 2*12 + 3*14, migration.moveCost(11, 3, 9));
+        assertEquals(3*6 + 3*9 + 2*12 + 3*14, (int) Math.ceil(migration.moveCost(11, 3, 9)));
         assertEquals(11, migration.reconfigTime(14, 3));
-        assertEquals(3*6 + 3*9 + 2*12 + 3*14, migration.moveCost(11, 14, 3));
+        assertEquals(3*6 + 3*9 + 2*12 + 3*14, (int) Math.ceil(migration.moveCost(11, 14, 3)));
     }
     
 }
