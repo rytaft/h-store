b27bc9026d9e6a74a65756f6fa990a1715a332ce
nosayba <nosayba@mit.edu>
2017-03-20 09:09:30 -0400

    Modified experiment script to skip first 30 min of benchmark time

--- src/frontend/org/qcri/affinityplanner/PredictiveController.java
+++ src/frontend/org/qcri/affinityplanner/PredictiveController.java
@@ -278,7 +278,7 @@ public class PredictiveController {
 
                 currentPlan = next_move.new_plan;
                 record("Moving to plan: " + currentPlan);
-                reconfig(currentPlan);
+//                reconfig(currentPlan);
 
                 try {
                     FileUtil.writeStringToFile(planFile, currentPlan);
