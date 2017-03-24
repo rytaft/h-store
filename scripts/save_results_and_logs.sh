65ed10dadd223f876e2909d9ef70959593e7ca05
Rebecca Taft <rytaft@istc13.(none)>
2017-03-24 09:18:48 -0400

    save all hevent logs

--- scripts/save_results_and_logs.sh
+++ scripts/save_results_and_logs.sh
@@ -6,3 +6,19 @@ cp -r obj/logs $1-logs
 cp results.csv $1
 cp out.log $1
 cp hevent.log $1
+
+for s in istc1 istc2 istc3 istc4 istc5 istc6 istc7 istc8 istc12 istc13
+do
+        echo "copying hevent.log from $s"
+        if [[ "$s" != "$HOSTNAME" ]]
+        then
+                scp $s:~/h-store/hevent.log $1/hevent-${s}.log
+                if [[ $? -ne 0 ]]
+                then
+                        echo "Failed to copy hevent.log from $s"
+                        echo "$?"
+                fi 
+        else
+            cp ~/h-store/hevent.log $1/hevent-${s}.log
+        fi
+done
