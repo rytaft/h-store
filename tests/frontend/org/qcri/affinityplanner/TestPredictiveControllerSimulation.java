937117b44620ededd3182d73c7e0cd1022e879d1
Rebecca Taft <rytaft@mit.edu>
2017-11-12 17:15:09 -0500

    fix capacity

--- tests/frontend/org/qcri/affinityplanner/TestPredictiveControllerSimulation.java
+++ tests/frontend/org/qcri/affinityplanner/TestPredictiveControllerSimulation.java
@@ -39,7 +39,7 @@ public class TestPredictiveControllerSimulation extends BaseTestCase {
     private static int MONITORING_TIME = 30000;  //(e.g. 3000 ms = 3 sec = 30 sec B2W-time)
     
     private static int FUDGE_FACTOR = 1;
-    private static long MAX_CAPACITY_PER_SERVER_PER_SEC = (long) Math.ceil(285 * FUDGE_FACTOR); // Q=350 txns/s
+    private static long MAX_CAPACITY_PER_SERVER_PER_SEC = (long) Math.ceil(438 * FUDGE_FACTOR); // Q=350 txns/s
     private static long MAX_CAPACITY_PER_SERVER = (long) Math.ceil(285 * FUDGE_FACTOR * MONITORING_TIME/1000.0); // Q=350 txns/s
     //public static long MAX_CAPACITY_PER_SERVER = (long) Math.ceil(23000 * FUDGE_FACTOR * MONITORING_TIME/1000.0); // Q=26000 txns/s
     private static int DB_MIGRATION_TIME = (int) Math.ceil(4646 * 1000.0/MONITORING_TIME); // D=4224 seconds + 10% buffer
